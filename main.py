70dff9308a0b2f255fbc5980f746f02e6a3bcc36
Jared Jones <jared.jones280@tamu.edu>
2021-10-18 17:08:37 -0500

    sorted out mailserver issues

--- main.py
+++ main.py
@@ -5,27 +5,29 @@ import email
 
 load_dotenv()
 #now have access to uid and pass and monitoremail
-
+#login to email server and setup
 host = 'imap.gmail.com'
 user = os.getenv("uid")
 password = os.getenv("pass")
 ssl = True
 
+#create server, login, and search inbox for messages
 server = IMAPClient(host, use_uid=True, ssl=ssl)
 server.login(user, password)
 server.select_folder("INBOX")
-messages = server.search(['NOT','DELETED', u'FLAGGED'])
+messages = server.search([['NOT','DELETED'], [u'FLAGGED']])
 
-for uid, message_data in server.fetch(messages, "RFC822").items():
+#get most recent messages [-1] (last item in list) and print info
+for uid, message_data in server.fetch(messages[-1], "RFC822").items():
         email_message = email.message_from_bytes(message_data[b"RFC822"])
-        print(uid, email_message.get("From"), email_message.get("Subject"))
 
+        #write subject line to file
+        #Debugging email content v
+        #print(uid, email_message.get("From"))
+        #print("Subject: ",email_message.get("Subject"))
+        #print("Body: \n",email_message.get_payload()[0])
 
-#for msgid, data in response.items():
-#
-#        parsedEmail = email.message_from_string('RFC822')
-#        body = email.message_from_string('BODY[TEXT]')
-#        #parsedBody = parsedEmail.get_payload(0)
-#        print(parsedEmail)
+        subject = email_message.get("Subject")
+        print(subject)
 
 server.logout()
